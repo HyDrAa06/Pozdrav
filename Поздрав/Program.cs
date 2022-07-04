e5d99361e0abe84568f7138716545dea8f1057d7
HyDrAa06 <108666340+HyDrAa06@users.noreply.github.com>
2022-07-04 14:47:45 +0300

    Add files via upload

---
+++ Поздрав/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Поздрав
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName, courseName;
+
+            Console.Write("Име на ученик: ");
+            studentName = Console.ReadLine();
+
+            Console.Write("Наименование на курс: ");
+            courseName = Console.ReadLine();
+
+            //работи само на английски
+            Console.WriteLine("Здравейте " + studentName + " и добре дошли в " + courseName + "!");
+          
+            Console.ReadLine();
+        }
+    }
+}
